631c2e3983490445f0a78ac7b1043f8827cb091d
jimmy4701 <jeremy.simonklein@gmail.com>
2024-10-23 18:01:56 +0200

    :lipstick: (SHOP ITEM USE IN PAGE) Used shop item component in page to display sh…

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 'use client'
 import { CookieZone } from "@/components/CookieZone";
+import { ShopItem } from "@/components/ShopItem";
 import { useMemo, useState } from "react";
 
 export interface ShopItemType {
+  id: number,
   image_url: string,
   label: string,
   price: number,
@@ -15,8 +17,8 @@ export default function Home() {
 
   const shopItems = useMemo<ShopItemType[]>(() => {
     return [
-      {label: "Mamy", image_url: "https://cdn-icons-png.flaticon.com/512/6247/6247428.png", price: 10, cps: 1},
-      {label: "Super Mamy", image_url: "https://cdn-icons-png.flaticon.com/512/6247/6247428.png", price: 100, cps: 10},
+      {id: 1, label: "Mamy", image_url: "https://cdn-icons-png.flaticon.com/512/6247/6247428.png", price: 10, cps: 1},
+      {id: 2, label: "Super Mamy", image_url: "https://cdn-icons-png.flaticon.com/512/6247/6247428.png", price: 100, cps: 10},
     ]
   }, [])
 
@@ -28,7 +30,9 @@ export default function Home() {
       <div className="center flex-1 bg-red-500">
       </div>
       <div className="right w-1/4 bg-yellow-500">
-      
+        {shopItems.map(item => 
+          <ShopItem item={item} key={item.id} onClick={() => { setCookies(cookies - item.price)}} />
+        )}
       </div>
     </div>
   );
--- src/components/ShopItem.tsx
+++ src/components/ShopItem.tsx
@@ -2,13 +2,13 @@ import { ShopItemType } from "@/app/page"
 import Image from "next/image"
 
 interface Props {
-    item: ShopItemType
+    item: ShopItemType,
 }
 
-export const ShopItem = ({item}:Props) => {
+export const ShopItem = ({item, ...props}:Props) => {
     return(
-        <div className="flex w-full p-2 border rounded-lg">
-            <Image src={item.image_url} alt={item.label} />
+        <div className="flex w-full p-2 border rounded-lg" {...props} >
+            <img className="h-20" src={item.image_url} alt={item.label} />
             <div className="flex flex-col p-2">
                 <h1>{item.label}</h1>
                 <p>Cout : {item.price} cookies</p>
