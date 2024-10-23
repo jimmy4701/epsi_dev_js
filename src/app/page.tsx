de275f22d24838c2c50185af01ee2056fac1b593
jimmy4701 <jeremy.simonklein@gmail.com>
2024-10-23 15:49:28 +0200

    :lipstick: (base style for home)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
-import Image from "next/image";
+'use client'
+import { useState } from "react";
 
 export default function Home() {
+
+  const [cookies, setCookies] = useState(0)
+
   return (
-    <div>
-      <p>HELLO !!!!!</p>
+    <div className="h-screen w-screen flex">
+      <div className="left w-1/4 bg-green-500">
+
+      </div>
+      <div className="center flex-1 bg-red-500">
+
+      </div>
+      <div className="right w-1/4 bg-yellow-500">
+      
+      </div>
     </div>
   );
 }
